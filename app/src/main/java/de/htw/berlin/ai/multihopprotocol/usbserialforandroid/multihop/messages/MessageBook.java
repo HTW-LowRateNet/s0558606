064efdc8574f7b71d583ebf196a7a70a87deac2f
Marcel Ebert <marcel@satoshipay.io>
2019-01-03 09:15:23 +0100

    change messageBook.hasMessage() to check for messages that are equal in originalsource, targetaddress and messageID

--- app/src/main/java/de/htw/berlin/ai/multihopprotocol/usbserialforandroid/multihop/messages/MessageBook.java
+++ app/src/main/java/de/htw/berlin/ai/multihopprotocol/usbserialforandroid/multihop/messages/MessageBook.java
@@ -9,28 +9,38 @@ public class MessageBook {
 
     private Set<MultihopMessage> messages;
 
-
     public MessageBook() {
         messages = new HashSet<>();
     }
 
-    public boolean hasMessage(MultihopMessage message) {
-        return messages.contains(message);
+    public boolean hasMessage(MultihopMessage newMessage) {
+        for (MultihopMessage message : messages) {
+            if (message.getMessageID().equals(newMessage.getMessageID())
+                    && message.getOriginalSourceAddress().equals(newMessage.getOriginalSourceAddress())
+                    && message.getTargetAddress().equals(newMessage.getTargetAddress())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean addMessage(MultihopMessage message) {
-        return messages.add(message);
+        if (!hasMessage(message)) {
+            return messages.add(message);
+        } else {
+            return false;
+        }
     }
 
     public boolean removeMessage(MultihopMessage message) {
         return messages.remove(message);
     }
 
-    public void clearAllMessagees() {
+    public void clearAllMessages() {
         messages.clear();
     }
 
-    public Collection<MultihopMessage> getAllMessagees() {
+    public Collection<MultihopMessage> getAllMessages() {
         return messages;
     }
 }
